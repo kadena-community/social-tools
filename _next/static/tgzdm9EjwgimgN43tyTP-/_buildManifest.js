4a2ec434f5ec8255a5d77be438aeeab1a87bba78
isaozler <isaozler@users.noreply.github.com>
2023-05-10 17:32:59 +0000

    deploy: 935f00f5bd9cbf1e67b59b3dc84b6daa4655bc51

---
+++ _next/static/tgzdm9EjwgimgN43tyTP-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1746a1404f0a7b48.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
